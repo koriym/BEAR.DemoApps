d1095a0ff6d74827ced2d83505c52e68dbe9f9ce
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-04-29 21:29:56 +0900

    autoloader for vendor/bear/demo-apps

--- Demo.Sandbox/bootstrap/autoload.php
+++ Demo.Sandbox/bootstrap/autoload.php
@@ -6,10 +6,8 @@ use BEAR\Package\Bootstrap\Bootstrap;
 
 
 $appDir = dirname(__DIR__);
-$packageDir = dirname(dirname(dirname(__DIR__)));
-$baseDir = file_exists($appDir . '/vendor/autoload.php') ? $appDir : $packageDir;
-$baseDir = $packageDir;
-$loader = require $baseDir . '/vendor/autoload.php';
+$packageDir = dirname(dirname(dirname(dirname(dirname(__DIR__)))));
+$loader = require $packageDir . '/vendor/autoload.php';
 
 // Hierarchical profiler @see http://www.php.net/manual/en/book.xhprof.php
 // require $packageDir . '/var/lib/profile.php';
