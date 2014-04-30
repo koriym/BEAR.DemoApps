241b1edc2783eaa0ca685e4d81dd509dfc2a658f
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-04-30 18:22:27 +0900

    did path for vendor/bear/demo-apps

--- Demo.Helloworld/bootstrap/autoload.php
+++ Demo.Helloworld/bootstrap/autoload.php
@@ -10,7 +10,7 @@ preload: {
 }
 
 autoload: {
-    $loader = require  dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php';
+    $loader = require  dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/vendor/autoload.php';
     /** @var $loader \Composer\Autoload\ClassLoader */
     $loader->addPsr4('Demo\Helloworld\\', dirname(__DIR__) . '/src');
     AnnotationRegistry::registerLoader([$loader, 'loadClass']);
