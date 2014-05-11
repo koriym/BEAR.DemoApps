5d1af680473a6ed638c11a2d31747043c4a6c2d3
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-12 03:06:54 +0900

    remove dev function load

--- Demo.Sandbox/tests/bootstrap.php
+++ Demo.Sandbox/tests/bootstrap.php
@@ -12,9 +12,6 @@ chdir(dirname(__DIR__));
 // load
 require_once 'bootstrap/autoload.php';
 
-// enable debug print p($var);
-(new Dev())->loadDevFunctions();
-
 // set the application path into the globals so we can access it in the tests.
 $_ENV['APP_DIR'] = dirname(__DIR__);
 
