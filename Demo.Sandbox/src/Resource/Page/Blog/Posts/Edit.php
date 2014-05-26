b59de1ff10c0b6655ec379239be8da3b6e98f924
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-27 03:42:44 +0900

    fix put posts

--- Demo.Sandbox/src/Resource/Page/Blog/Posts/Edit.php
+++ Demo.Sandbox/src/Resource/Page/Blog/Posts/Edit.php
@@ -45,7 +45,6 @@ class Edit extends ResourceObject
      */
     public function onPut($id, $title, $body)
     {
-        $this['id'] = $id;
         // update post
         $updateUri = $this->links['update'][Link::HREF];
         $this->resource
@@ -54,7 +53,8 @@ class Edit extends ResourceObject
             ->withQuery(['id' => $id, 'title' => $title, 'body' => $body])
             ->eager
             ->request();
-
+        $this['id'] = $id;
+        $this['submit'] = ['title' => $title, 'body' => $body];
         // redirect
         $this->code = Code::SEE_OTHER;
         $this->headers = ['Location' => $this->links['next'][Link::HREF]];
